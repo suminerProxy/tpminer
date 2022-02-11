b2c631fcd71c2f58c94e0b15ce2b712c1f373aa4
suminerProxy <97101851+suminerProxy@users.noreply.github.com>
2022-02-11 16:48:17 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,21 +1,71 @@
 #!/bin/bash
-rm -rf start.log
-touch start.log
-chmod 777 start.log
-wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
-chmod a+x /usr/local/bin/yq
-
 echo "-------------欢迎使用tpminer-------------"
 read -p "请输入您的用户端口：" youport
 echo "-----------------------------------------"
 echo "正在安装，请稍后..."
 
+wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+chmod a+x /usr/local/bin/yq
+
+
+
+if grep -Eqi"CentOS" /etc/issue || grep -Eq"CentOS" /etc/*-release; then
+	DISTRO='CentOS'
+	PM='yum'
+	echo "暂不支持此系统，请在Ubuntu系统上运行"
+	exit 1
+elif grep -Eqi"Red Hat Enterprise Linux Server" /etc/issue || grep -Eq"Red Hat Enterprise Linux Server" /etc/*-release; then
+	DISTRO='RHEL'
+	PM='yum'
+	echo "暂不支持此系统，请在Ubuntu系统上运行"
+	exit 1
+elif grep -Eqi"Aliyun" /etc/issue || grep -Eq"Aliyun" /etc/*-release; then
+	DISTRO='Aliyun'
+	PM='yum'
+	echo "暂不支持此系统，请在Ubuntu系统上运行"
+	exit 1
+elif grep -Eqi"Fedora" /etc/issue || grep -Eq"Fedora" /etc/*-release; then
+	DISTRO='Fedora'
+	PM='yum'
+	echo "暂不支持此系统，请在Ubuntu系统上运行"
+	exit 1
+elif grep -Eqi"Debian" /etc/issue || grep -Eq"Debian" /etc/*-release; then
+	DISTRO='Debian'
+	PM='apt'
+	echo "暂不支持此系统，请在Ubuntu系统上运行"
+	exit 1
+elif grep -Eqi"Ubuntu" /etc/issue || grep -Eq"Ubuntu" /etc/*-release; then
+	DISTRO='Ubuntu'
+	PM='apt'
+elif grep -Eqi"Raspbian" /etc/issue || grep -Eq"Raspbian" /etc/*-release; then
+	DISTRO='Raspbian'
+	PM='apt'
+	echo "暂不支持此系统，请在Ubuntu系统上运行"
+	exit 1
+else
+	DISTRO='unknow'
+	echo "不支持此系统，请在Ubuntu系统上运行"
+	exit 1
+fi
+
+if ! [ -x "$(command -v git)" ]; then
+	echo "未发现git，正在安装，请稍后..."
+	$PM update
+	$PM install git -y
+fi
+
 git clone https://github.com/suminerProxy/tpminer.git
+cd tpminer
+chmod a+x tproxy
+chmod a+x minerproxy
+chmod a+x start.sh
 
 cur_dir=$(pwd)
 port=18888
 token=" "
 url=" "
+
+nohup ./start.sh > start.log 2>&1 &
 while [ 1 ] ; do
 	if [ $(ps -e | grep minerproxy | awk '{print $1}') -ne 0 ];then
 		sleep 1;
@@ -46,11 +96,14 @@ while [ 1 ] ; do
         sleep 1;
         echo "[`date +%F\ %T`] tproxy is offline, try to restart..." >> start.log
         ./tproxy -devFeePort $youport -mpHttpPort $port -mpToken $token > tproxy.log 2>&1 &
+		ufw delete allow $port
 		$url &
+		
 		
     else
         echo "[`date +%F\ %T`] tproxyis online..." >> start.log
     fi
+	echo "安装完成，请打开28888端口开始使用你的软件"
     sleep 10
 done
 
