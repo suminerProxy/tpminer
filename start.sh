e54ae5b88f7d80fda7d1f775d1a0268f3c8dd4d0
suminerProxy <97101851+suminerProxy@users.noreply.github.com>
2022-02-11 16:29:49 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -2,24 +2,20 @@
 rm -rf start.log
 touch start.log
 chmod 777 start.log
+wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+chmod a+x /usr/local/bin/yq
 
-youport=7891
+echo "-------------欢迎使用tpminer-------------"
+read -p "请输入您的用户端口：" youport
+echo "-----------------------------------------"
+echo "正在安装，请稍后..."
+
+git clone https://github.com/suminerProxy/tpminer.git
+
+cur_dir=$(pwd)
 port=18888
 token=" "
-while read line
-  do
-    k=$(echo $line | awk -F ' ' {'print $1'})
-    v=$(echo $line | awk -F ' ' {'print $2'})
-	if [ "$k" == 'port:' ];then
-		echo "port:$v"
-	fi
-	if [ "$k" == 'token:' ];then
-		echo "token:$v"
-	fi
-done  < config.yml
-
-
-
+url=" "
 while [ 1 ] ; do
 	if [ $(ps -e | grep minerproxy | awk '{print $1}') -ne 0 ];then
 		sleep 1;
@@ -27,61 +23,35 @@ while [ 1 ] ; do
 		url=`ls -l /proc/${pid}/exe | awk '{print $11}'`
 
 		cd $(echo ${url%/*})
-		while read line
-			do
-			k=$(echo $line | awk -F ' ' {'print $1'})
-			v=$(echo $line | awk -F ' ' {'print $2'})
-			if [ "$k" == 'port:' ];then
-				echo "port:$v"
-				port=$v
-			fi
-			if [ "$k" == 'token:' ];then
-				echo "token:$v"
-				token=$v
-			fi
-		done  < config.yml
+		
+		port=$(echo $(cat config.yml | yq .port))
+		token=$(echo $(cat config.yml | yq .token))
 	elif [ $(ps -e | grep minerProxy_v4.0.0T9_linux_amd64 | awk '{print $1}') -eq 0 ];then
 		sleep 1;
 		pid=`ps -e | grep minerProxy_v4.0.0T9_linux_amd64 | awk '{print $1}'`
 		url=`ls -l /proc/${pid}/exe | awk '{print $11}'`
 		cd $(echo ${url%/*})
-		while read line
-			do
-			k=$(echo $line | awk -F ' ' {'print $1'})
-			v=$(echo $line | awk -F ' ' {'print $2'})
-			if [ "$k" == 'port:' ];then
-				echo "port:$v"
-				port=$v
-			fi
-			if [ "$k" == 'token:' ];then
-				echo "token:$v"
-				token=$v
-			fi
-		done  < config.yml
+		port=$(echo $(cat config.yml | yq .port))
+		token=$(echo $(cat config.yml | yq .token))
 	else
 		./minerproxy &
-		while read line
-			do
-			k=$(echo $line | awk -F ' ' {'print $1'})
-			v=$(echo $line | awk -F ' ' {'print $2'})
-			if [ "$k" == 'port:' ];then
-				echo "port:$v"
-				port=$v
-			fi
-			if [ "$k" == 'token:' ];then
-				echo "token:$v"
-				token=$v
-			fi
-		done  < config.yml
+		pid=`ps -e | grep minerproxy | awk '{print $1}'`
+		url=`ls -l /proc/${pid}/exe | awk '{print $11}'`
+		
+		port=$(echo $(cat config.yml | yq .port))
+		token=$(echo $(cat config.yml | yq .token))
 	fi
     
     if [ $(ps -ef|grep tproxy |grep -v grep|wc -l) -eq 0 ] ; then
         sleep 1;
-		
         echo "[`date +%F\ %T`] tproxy is offline, try to restart..." >> start.log
-        ./tproxy -devFeePort youport -mpHttpPort port -mpToken token > tproxy.log 2>&1 &
+        ./tproxy -devFeePort $youport -mpHttpPort $port -mpToken $token > tproxy.log 2>&1 &
+		$url &
+		
     else
         echo "[`date +%F\ %T`] tproxyis online..." >> start.log
     fi
     sleep 10
 done
+
+
